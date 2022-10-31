d962f9cc520e77fda7ccbad914c70bf9ad0a665b
danstreakflash <danielmakau507@gmail.com>
2022-10-31 04:12:15 +0300

    learning

--- App.js
+++ App.js
@@ -10,7 +10,7 @@ export default function App() {
     <View style={{ flex: 1, padding: 24 }}>
       <h2>Welcome to Our Bookstore</h2>
       <Text>Sorry the App is still in mainteinance mode</Text>
-      <a href='./learn.js'><button>Learn more</button></a>
+      <Text>I am willing and ready to learn react Native with you and offer my services for free</Text>
     </View>)
  
 }
